c31c354e0780b35443f5fdc9936fdeb02d38090e
youssef12001 <45400001+youssef12001@users.noreply.github.com>
2018-11-28 00:31:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,3 +33,33 @@ client.user.setGame(`LastCodes4Ever  `,"https://www.twitch.tv/dggamingbot")
  
  
 client.login(process.env.BOT_TOKEN);
+
+
+
+client.on('message', message => {
+var prefix = "$";
+       if(message.content === prefix + "cl") {
+                           if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ظ„ظٹط³ ظ„ط¯ظٹظƒ طµظ„ط§ط­ظٹط§طھ__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: false
+
+              }).then(() => {
+                  message.reply("**__طھظ… طھظ‚ظپظٹظ„ ط§ظ„ط´ط§طھ__ :white_check_mark: **")
+              });
+                }
+//FIRE BOT
+    if(message.content === prefix + "op") {
+                        if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ظ„ظٹط³ ظ„ط¯ظٹظƒ طµظ„ط§ط­ظٹط§طھ__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: true
+
+              }).then(() => {
+                  message.reply("**__طھظ… ظپطھط­ ط§ظ„ط´ط§طھ__:white_check_mark:**")
+              });
+    }
+       
+});
